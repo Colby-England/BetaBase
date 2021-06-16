bb6994f1c3176251264e61f3df9192a9d15d9c22
Colby England <englandcolby@gmail.com>
2021-06-16 13:52:53 -0500

    Initial Commit

---
+++ public/js/gyms.js
@@ -0,0 +1,33 @@
+// const baseURL = "http://localhost:3625/gyms"
+// const baseURL = "http://localhost:3000/gyms"
+
+// document.addEventListener('DOMContentLoaded', addRow)
+
+// function addRow() {
+//     document.getElementById('add_gym').addEventListener('click', () => addGym(), false);
+// }
+
+// async function addGym() {
+//     let name = document.getElementById('name').value
+//     let city = document.getElementById('city').value
+//     let state = document.getElementById('state').value
+//     let address = document.getElementById('address').value
+//     let cost = document.getElementById('cost').value
+
+//     let payload = { name: null, city: null, state: null, address: null, cost: null };
+//         payload.name = name
+//         payload.city = city
+//         payload.stateID = state
+//         payload.address = `${address}`
+//         payload.cost = `${cost}`
+    
+//     console.log(payload)
+
+    // axios.post(baseURL, payload)
+
+    // .then((response) => {
+    //     console.log(response);
+    // }, (error) => {
+    //     console.log(error);
+    // });
+// };
